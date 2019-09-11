98661bcf0f010a42a7afa0dfd6316ceca0994721
oksana-gorovaya <ksenia15grv@gmail.com>
2019-09-11 14:25:11 +0300

    Task 3.6 https://stepik.org/lesson/127928/step/5?unit=102085

---
+++ NovelWithTable.py
@@ -0,0 +1,31 @@
+from BookErrors import *
+from Novel import Novel
+
+
+class NovelWithTable(Novel):
+
+    def __init__(self, author, year, title, content=[], table={}):
+        super().__init__(author, year, title)
+        self.author = author
+        self.year = year
+        self.title = title
+        self.content = content
+        self.table = table
+        self.size = len(self.content)
+
+    def search(self, chapter_name):
+        try:
+            return self.table[chapter_name]
+        except:
+            raise PageNotFoundError
+
+    def add_chapter(self, chapter_name, page):
+        if chapter_name in self.table.keys():
+            raise Exception('Duplicate chapter')
+        return self.table.update({chapter_name: page})
+
+    def remove_chapter(self, chapter_name):
+        try:
+            del self.table[chapter_name]
+        except:
+            raise PageNotFoundError
